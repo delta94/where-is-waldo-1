777b838f174796855f4fdf2fdef2dd3371253624
osechi3 <holypoetwrk@gmail.com>
2020-09-26 12:55:13 +0300

    Moving firebaseConfig into a separate file

--- src/app.js
+++ src/app.js
@@ -1,19 +1,10 @@
 import * as firebase from 'firebase/app'
 import PubSub from 'pubsub-js'
 
+import { firebaseConfig } from '../firebase.config'
 import './styles/style.css'
 import { View } from './components/View'
 import { Model } from './components/Model'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCfRM7X4m5le2L27z6kyux-gheNQTxxwOA',
-  authDomain: 'where-is-waldo-odin.firebaseapp.com',
-  databaseURL: 'https://where-is-waldo-odin.firebaseio.com',
-  projectId: 'where-is-waldo-odin',
-  storageBucket: 'where-is-waldo-odin.appspot.com',
-  messagingSenderId: '453350345911',
-  appId: '1:453350345911:web:b5801a38742a1d602dd132'
-}
 
 firebase.initializeApp(firebaseConfig)
 
--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -1,17 +1,9 @@
 import * as firebase from 'firebase/app'
 import 'firebase/firestore'
 import PubSub from 'pubsub-js'
-import '../../styles/leaderboard.css'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCfRM7X4m5le2L27z6kyux-gheNQTxxwOA',
-  authDomain: 'where-is-waldo-odin.firebaseapp.com',
-  databaseURL: 'https://where-is-waldo-odin.firebaseio.com',
-  projectId: 'where-is-waldo-odin',
-  storageBucket: 'where-is-waldo-odin.appspot.com',
-  messagingSenderId: '453350345911',
-  appId: '1:453350345911:web:b5801a38742a1d602dd132'
-}
+import { firebaseConfig } from '../../../firebase.config'
+import '../../styles/leaderboard.css'
 
 firebase.initializeApp(firebaseConfig)
 
