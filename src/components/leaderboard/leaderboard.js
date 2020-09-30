98ab7e00d4e95292476ddb6f49677ce8edce33ea
osechi3 <holypoetwrk@gmail.com>
2020-09-30 11:33:05 +0300

    Displaying the leaderboard depending on the chosen level

--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -10,17 +10,40 @@ firebase.initializeApp(firebaseConfig)
 class Leaderboard {
   constructor () {
     this.dataLeaderboard = []
-    this.getLeaderboardDataFromServer()
+    this.initListeners()
+    this.getLeaderboardDataFromServer('level-1')
+  }
 
+  initListeners () {
     /* Displaying the data from the server in the DOM */
     PubSub.subscribe('data_leaderboard_loaded', () => {
       this.displayDataFromServer()
     })
+
+    const buttonsSelectLevel = document.querySelectorAll('.btn-select-level')
+    buttonsSelectLevel.forEach(button => {
+      /* Removing the old data and loading the selected-level data */
+      button.addEventListener('click', () => {
+        this.removeData()
+        this.removeDataFromDOM()
+        this.getLeaderboardDataFromServer(button.dataset.level)
+        this.displayDataFromServer()
+      })
+
+      /* Highlighting the clicked button only */
+      button.addEventListener('click', (e) => {
+        buttonsSelectLevel.forEach(button => {
+          button.classList.remove('btn-toggled')
+        })
+        e.currentTarget.classList.add('btn-toggled')
+      })
+    })
   }
 
-  async getLeaderboardDataFromServer () {
+  async getLeaderboardDataFromServer (levelId) {
     const collectionLeaderboard =
       firebase.firestore().collection('leaderboard')
+        .doc(levelId).collection('leaderboard')
         .orderBy('time')
         .limit(20)
 
@@ -43,6 +66,21 @@ class Leaderboard {
       listEntry.lastElementChild.textContent =
         `Has finished in ${entry.time} seconds`
       listPlayers.append(listEntry)
+    })
+  }
+
+  removeData () {
+    this.dataLeaderboard = []
+  }
+
+  removeDataFromDOM () {
+    const listPlayers = document.getElementById('leaderboard-list-players')
+    const itemsList = listPlayers.querySelectorAll('.list-item')
+
+    itemsList.forEach(item => {
+      if (item.id !== 'list-template') {
+        item.remove()
+      }
     })
   }
 }
