2d4fdf2fca9e7b681395430842720a165446052d
osechi3 <holypoetwrk@gmail.com>
2020-09-30 12:07:07 +0300

    Disabling 'compact' option of Babel in the development build

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
         test: /\.js$/,
         exclude: '/node_modules/',
         use: [
-          'babel-loader',
+          'babel-loader?compact=false',
           'eslint-loader'
         ]
       }
