35e1a8d45055288c78f14baa1cf1eda262b7c420
osechi3 <holypoetwrk@gmail.com>
2020-10-02 12:17:28 +0300

    Deploy to gh-pages

--- firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCfRM7X4m5le2L27z6kyux-gheNQTxxwOA',
-  authDomain: 'where-is-waldo-odin.firebaseapp.com',
-  databaseURL: 'https://where-is-waldo-odin.firebaseio.com',
-  projectId: 'where-is-waldo-odin',
-  storageBucket: 'where-is-waldo-odin.appspot.com',
-  messagingSenderId: '453350345911',
-  appId: '1:453350345911:web:b5801a38742a1d602dd132'
-}
